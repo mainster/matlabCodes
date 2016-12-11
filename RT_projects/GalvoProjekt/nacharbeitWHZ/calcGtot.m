d136c54636f14574dd350c84cd9ba46f854177b2
Manuel Del Basso <manuel.delbasso@gmail.com>
2016-12-12 00:20:56 +0100

    New strategy for the distribution of model parameters implemented

--- RT_projects/GalvoProjekt/nacharbeitWHZ/calcGtot.m
+++ RT_projects/GalvoProjekt/nacharbeitWHZ/calcGtot.m
@@ -1,53 +1,49 @@
 %% Calculate Gtot (1)
 % Calculate tf Gtot by subsystem tfs
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-clear KEL KTB KFR KEMF JR RL L s
-syms KEL KTB KFR KEMF JR RL L s
+vars=[KEL KTB KFR KEMF JR RL L];
+clear vars s
+syms vars s
 
-fprintf('\n\n')
+fprintf('\n%s\n%s\n',ds,ds);
 G1=1/(s*L+RL);
-G1=(1/(s*L))/(1+RL/(s*L));
 G2=1/(s*JR+KFR);
 G3=G1*KEL;
 G4=G2/(1+G2*KTB/s);
 G5=G3*G4/(1+G3*G4*KEMF);
 G6=G5/s;
 
-Gt=collect(G6,s);
-pretty(Gt)
+Gsym=[G1; G2; G3; G4; G5; G6];
+for k=1:length(Gsym)
+    Gsym(k)=collect(simplify(Gsym(k), 'steps', k*10), s);
+    pretty(Gsym(k));
+end
 
 %% Load Galvo parameters (2)
-% Load parameter set and derive a param structure
+% Load parameters from data dictionary file
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+DATA_DICT='modelDesignData_Galvo_sys_Ccont_Pcont.sldd';
+ddo=Simulink.data.dictionary.open(DATA_DICT);
+dds=ddo.getSection('Design Data');
+
+%% Create transfer function (3)
+% Create tf by subs symbolic equation from (1)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-if 0
-    P=loadGalvoParam(98);
-    p=struct;
-    
-    for k=1:length(P)
-        p.(P{k,1}).v=P{k,2};
-        p.(P{k,1}).unit=P{k,3};
-        p.(P{k,1}).desc=P{k,4};
-    end
-    Gts=subs(Gt,...
-        {'KEL' 'KTB' 'KFR' 'KEMF' 'JR' 'RL' 'L'},...
-        {p.KEL.v, p.KTB.v, p.KFR.v, p.KEMF.v, p.JR.v, p.RL.v, p.L.v});
-    
-else
-    p=load('ddict.mat');
-    
-    Gts=subs(Gt,...
-        {'KEL' 'KTB' 'KFR' 'KEMF' 'JR' 'RL' 'L'},...
-        {p.KEL.Value, p.KTB.Value, p.KFR.Value, p.KEMF.Value, p.JR.Value,...
-        p.RL.Value, p.L.Value});
-    Gts1=subs(G1,...
-        {'KEL' 'KTB' 'KFR' 'KEMF' 'JR' 'RL' 'L'},...
-        {p.KEL.Value, p.KTB.Value, p.KFR.Value, p.KEMF.Value, p.JR.Value,...
-        p.RL.Value, p.L.Value});
-end
-%% Create transfer function (3)
-% Create tf based on (1)
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Get all sym variables and Remove non-replaceable symbolic variable 's'
+svs=symvar(Gsym);
+svs=svs(svs~=s);
+svstr=arrayfun(@char, svs, 'UniformOutput', false);
+
+% Create numeric vector for symbol substitution
+numvec=zeros(size(svs));
+for k=1:length(svs); 
+     numvec(k)=dds.getEntry(svstr{k}).getValue.Value;
+end    
+
+% Substitution of syms, except s
+Gnum=vpa(subs(Gsym, svs, num2cell(numvec)), 5);
+
 s=tf('s');
 [num,den]=numden(Gts);
 Gts=tf(double(coeffs(num,'s')), double(coeffs(den,'s')))
@@ -55,11 +51,11 @@ Gts=tf(double(coeffs(num,'s')), double(coeffs(den,'s')))
 %%
 p=load('ddict.mat');
 
-    Gts1=subs(G1,...
-        {'KEL' 'KTB' 'KFR' 'KEMF' 'JR' 'RL' 'L'},...
-        {p.KEL.Value, p.KTB.Value, p.KFR.Value, p.KEMF.Value, p.JR.Value,...
-        p.RL.Value, p.L.Value});
-    
+Gts1=subs(G1,...
+    {'KEL' 'KTB' 'KFR' 'KEMF' 'JR' 'RL' 'L'},...
+    {p.KEL.Value, p.KTB.Value, p.KFR.Value, p.KEMF.Value, p.JR.Value,...
+    p.RL.Value, p.L.Value});
+
 [num,den]=numden(Gts1);
 Gts1=tf(double(coeffs(num,'s')), double(coeffs(den,'s')))
 
